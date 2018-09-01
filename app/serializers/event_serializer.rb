8ba5ea76c5b7678abfe0554420a857558c8f1b59
Martin Fenner <martin.fenner@datacite.org>
2018-09-01 19:08:59 +0200

    enable caching for events

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -3,7 +3,7 @@ class EventSerializer
   set_key_transform :dash
   set_type :events
   set_id :uuid
-  # cache_options enabled: true, cache_length: 24.hours
+  cache_options enabled: true, cache_length: 24.hours
   
   attributes :message_action, :source_token, :relation_type_id, :source_id, :total, :license, :occurred_at, :timestamp
   
