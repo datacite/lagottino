4c956944e8bc0a86865a0021b28bba27d2cc6d22
Martin Fenner <martin.fenner@datacite.org>
2018-09-01 23:28:44 +0200

    polish serializers

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -3,12 +3,12 @@ class EventSerializer
   set_key_transform :dash
   set_type :events
   set_id :uuid
-  #cache_options enabled: true, cache_length: 24.hours
+  cache_options enabled: true, cache_length: 24.hours
   
   attributes :source_id, :relation_type_id, :total, :message_action, :source_token, :license, :occurred_at, :timestamp
   
-  belongs_to :subj, serializer: ObjectSerializer, record_type: :objects #, if: Proc.new { |record| record.subj && record.subj["uid"] }
-  belongs_to :obj, serializer: ObjectSerializer, record_type: :objects #, if: Proc.new { |record| record.obj && record.obj["uid"] }
+  belongs_to :subj, serializer: ObjectSerializer, record_type: :objects #, if: Proc.new { |record| record.subj.respond_to?(:id) }
+  belongs_to :obj, serializer: ObjectSerializer, record_type: :objects #, if: Proc.new { |record| record.obj.respond_to?(:id) }
 
   attribute :timestamp, &:updated_at
 end
--- app/serializers/object_serializer.rb
+++ app/serializers/object_serializer.rb
@@ -2,8 +2,17 @@ class ObjectSerializer
   include FastJsonapi::ObjectSerializer
   set_key_transform :dash
   set_type :objects
-  set_id :uid
   cache_options enabled: true, cache_length: 24.hours
   
-  attributes :name, :author, :periodical, :volume_number, :issue_number, :pagination, :publisher, :issn, :version, :date_published
+  attributes :subtype, :name, :author, :periodical, :volume_number, :issue_number, :pagination, :publisher, :issn, :version, :date_published
+
+  attribute :subtype do |object|
+    object.type
+  end
+
+  attribute :author do |object|
+    object.author.map do |a|
+      a.present? ? a.transform_keys! { |key| key.tr('_', '-') } : nil
+    end.compact
+  end
 end
