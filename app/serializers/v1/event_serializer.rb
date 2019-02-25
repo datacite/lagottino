5183f5d54d4a2a1389f331c0c5c8b67f7ade212d
Martin Fenner <martin.fenner@datacite.org>
2019-02-25 07:45:06 +0100

    version object serializer. #26

--- app/serializers/v1/event_serializer.rb
+++ app/serializers/v1/event_serializer.rb
@@ -6,8 +6,8 @@ class V1::EventSerializer
   
   attributes :subj_id, :obj_id, :source_id, :relation_type_id, :total, :message_action, :source_token, :license, :occurred_at, :timestamp
   
-  belongs_to :subj, serializer: ObjectSerializer, record_type: :objects
-  belongs_to :obj, serializer: ObjectSerializer, record_type: :objects
+  belongs_to :subj, serializer: V1::ObjectSerializer, record_type: :objects
+  belongs_to :obj, serializer: V1::ObjectSerializer, record_type: :objects
 
   attribute :timestamp, &:updated_at
 end
--- app/serializers/v1/object_serializer.rb
+++ app/serializers/v1/object_serializer.rb
@@ -1,8 +1,7 @@
-class ObjectSerializer
+class V1::ObjectSerializer
   include FastJsonapi::ObjectSerializer
-  set_key_transform :camel_lower
+  set_key_transform :dash
   set_type :objects
-  cache_options enabled: true, cache_length: 24.hours
   
   attributes :subtype, :name, :author, :periodical, :volume_number, :issue_number, :pagination, :publisher, :issn, :version, :date_published
 
